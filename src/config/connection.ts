6c4c9dfd3e1830e9b88730651f5440a8d222352d
devadmin007 <dev.admin@technithunder.com>
2025-06-16 17:22:31 +0530

    Update connection.ts

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -3,7 +3,7 @@ import { config } from './index';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://admin:admin@123@cluster0.o4pzn.mongodb.net/employee-management");
+    await mongoose.connect("mongodb+srv://admin:l24r2UmrNAj9OlxV@cluster0.o4pzn.mongodb.net/employee-management");
     console.log(`MongoDB connected to ${config.mongoUri}`);
   } catch (error) {
     console.error('MongoDB connection error:', error);
