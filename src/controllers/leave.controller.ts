fbd13252a5f1cbd96286249eb6de12f593bcd5e8
keyur modi <keyrumodi2508@gmail.com>
2025-06-26 18:19:52 +0530

    change in the  leave list

--- src/controllers/leave.controller.ts
+++ src/controllers/leave.controller.ts
@@ -155,10 +155,10 @@ export const leaveList = async (req: any, res: Response) => {
     }
 
     if (status) match.status = status;
-    if (startDate) match.startDate = { $gte: new Date(startDate as string) };
-    if (endDate) {
-      match.endDate = match.endDate || {};
-      match.endDate.$lte = new Date(endDate as string);
+    if (startDate || endDate) {
+      match.date = {};
+      if (startDate) match.date.$gte = new Date(startDate as string);
+      if (endDate) match.date.$lte = new Date(endDate as string);
     }
 
     const { search } = req.query;
@@ -189,10 +189,10 @@ export const leaveList = async (req: any, res: Response) => {
       },
       {
         $addFields: {
-          employeefullName: {
+          employee_full_name: {
             $concat: ["$employeeId.firstName", " ", "$employeeId.lastName"]
           },
-          approverfullName: {
+          approver_full_name: {
             $concat: ["$approveId.firstName", " ", "$approveId.lastName"]
           },
         },
@@ -204,8 +204,8 @@ export const leaveList = async (req: any, res: Response) => {
       pipeline.push({
         $match: {
           $or: [
-            { employeefullName: { $regex: search, $options: "i" } },
-            { approverfullName: { $regex: search, $options: "i" } },
+            { employee_full_name: { $regex: search, $options: "i" } },
+            { approver_full_name: { $regex: search, $options: "i" } },
           ],
         },
       });
@@ -223,8 +223,8 @@ export const leaveList = async (req: any, res: Response) => {
           updatedAt: 1,
           leave_type: 1,
           totalDays: 1,
-          approverfullName: 1,
-          employeefullName: 1,
+          employee_full_name: 1,
+          approver_full_name: 1,
         },
       },
       { $sort: sort },
